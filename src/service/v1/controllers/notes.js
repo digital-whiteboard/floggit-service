836de7d451e5f518824e9ba204aa0536aea2cef9
den4is <d.piric@hotmail.com>
2017-09-22 02:07:53 +0200

    #1 scaffolding

---
+++ src/service/v1/controllers/notes.js
@@ -0,0 +1,30 @@
+const todoItems = require('../../../repository/notes');  // <--
+
+const get = (req, res) => {
+
+};
+
+const getAll = (req, res) => {
+
+};
+
+const remove = (req, res) => {
+
+};
+
+const add = (req, res) => {
+
+};
+
+
+const update = (req, res) => {
+
+};
+
+module.exports = {
+  get,
+  getAll,
+  remove,
+  add,
+  update,
+};
