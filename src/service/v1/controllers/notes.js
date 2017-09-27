b0a109f2e1923556484573e3e8cc640b2a3667da
den4is <d.piric@hotmail.com>
2017-09-27 13:23:07 +0200

    fixed eslint style

--- src/service/v1/controllers/notes.js
+++ src/service/v1/controllers/notes.js
@@ -1,24 +1,31 @@
-const todoItems = require('../../../repository/notes');  // <--
+const notes = require('../../../repository/notes');
 
 const get = (req, res) => {
-
+  const id = req.params.id;
+  const note = notes.get(id);
+  if (note) {
+    res.json(notes.get(id));
+  } else {
+    res.status(404).json({
+      type: 'error',
+      message: `Note ${id} not found` });
+  }
 };
 
 const getAll = (req, res) => {
-
+  res.json(notes.getAll());
 };
 
 const remove = (req, res) => {
-
+  const id = req.params.id;
+  notes.remove(id);
+  res.status(204).send();
 };
 
 const add = (req, res) => {
-
-};
-
-
-const update = (req, res) => {
-
+  const note = req.body.value;
+  const id = notes.add(note);
+  res.status(201).json({ id });
 };
 
 module.exports = {
@@ -26,5 +33,4 @@ module.exports = {
   getAll,
   remove,
   add,
-  update,
 };
